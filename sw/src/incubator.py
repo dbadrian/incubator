1e0c4a456b562f737c115912ab81f4c81e8b7faa
David B. Adrian <dawidh.adrian@googlemail.com>
2018-09-29 16:27:09 +0200

    ambient sensor logger debug output

--- sw/src/incubator.py
+++ sw/src/incubator.py
@@ -82,7 +82,7 @@ def run(args):
     # todo: hardcoded, make agnostic
     print(":: Setting Up Sensors")
     ambient_sensors = [DHT(s["type_id"], s['gpio_data']) for s in cfg["ambient_sensors"]]
-    # logger.debug(ambient_sensors)
+    logger.debug(ambient_sensors)
     food_sensors = DS18S20()
 
     # control loop # TODO:limit Hz
