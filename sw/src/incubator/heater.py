2d872fdfc8173c16e5350dc978d3c79e3f96144e
David B. Adrian <dawidh.adrian@googlemail.com>
2018-09-29 12:53:42 +0200

    update heater class to softpwm

--- sw/src/incubator/heater.py
+++ sw/src/incubator/heater.py
@@ -18,26 +18,27 @@ class Haljia5V8W():
         self.fan = Fan(gpio_pin_fan)
 
         self.heater_state = 0
-        wiringpi.pinMode(self.gpio_pin_heater, wiringpi.GPIO.PWM_OUTPUT)
+        wiringpi.pinMode(self.gpio_pin_heater, wiringpi.GPIO.OUTPUT)
+        wiringpi.softPwmCreate(self.gpio_pin_heater ,0, 100)
         wiringpi.pullUpDnControl(self.gpio_pin_heater, wiringpi.GPIO.PUD_DOWN)
         # Set to slowest possible level
-        wiringpi.pwmSetClock(self.PWM_CLOCK)
-        wiringpi.pwmSetRange(self.PWM_RANGE)
+        # wiringpi.pwmSetClock(self.PWM_CLOCK)
+        # wiringpi.pwmSetRange(self.PWM_RANGE)
 
         #put everything in a sane state...just in case
         self.heater_off()
 
     def heater_on(self, heat_level):
-        # we always switch on the fan, this way we can have the fan runnignb
+        # we always switch on the fan, this way we can have the fan running
         # even if the set the heat_level to zero (no heating)
         self.fan.on()
 
         if heat_level >= 0 and heat_level <= 100:
-            self.heater_state = int(heat_level * 4096 / 100)
-            wiringpi.pwmWrite(self.gpio_pin_heater, self.heater_state)
+            # self.heater_state = int(heat_level * 4096 / 100)
+            wiringpi.softPwmWrite(self.gpio_pin_heater, self.heater_state)
 
     def heater_off(self):
         # todo: should be a property
         self.heater_state = 0
-        wiringpi.pwmWrite(self.gpio_pin_heater, self.heater_state)
+        wiringpi.softPwmWrite(self.gpio_pin_heater, self.heater_state)
         self.fan.off()
