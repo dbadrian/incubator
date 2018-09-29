3e8cfeffe7bd1e66c1ee289d6a6f02f0f5db4963
David B. Adrian <dawidh.adrian@googlemail.com>
2018-09-29 16:02:31 +0200

    minor fixes for testing

--- sw/src/incubator.py
+++ sw/src/incubator.py
@@ -80,7 +80,8 @@ def run(args):
     time_prev_it = time_start
 
     # todo: hardcoded, make agnostic
-    ambient_sensors = [DHT("22", s['gpio_data']) for s in cfg["ambient_sensors"]]
+    ambient_sensors = [DHT(s["type_id"], s['gpio_data']) for s in cfg["ambient_sensors"]]
+    logger.debug(ambient_sensors)
     food_sensors = DS18S20()
 
     # control loop # TODO:limit Hz
